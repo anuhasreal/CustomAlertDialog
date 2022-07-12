969f22f6439ef6cdd9469c02f6ba5e8ba9222925
AnuhasRox <85325274+AnuhasRox@users.noreply.github.com>
2022-07-12 17:55:29 +0530

    Create AlertDialog.java

---
+++ AlertDialog.java
@@ -0,0 +1,18 @@
+private void launchAlertDialog() {
+        new MaterialAlertDialogBuilder(Project.this, R.style.MaterialAlertDialog_Rounded)
+                .setTitle("Quity update required")
+                .setMessage("Quity recommends that you to update to latest version")
+                .setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        try {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.anuhas.quity")));
+                        }catch (Exception e){
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.anuhas.quity")));
+                        }
+                    }
+                })
+                .setCancelable(false)
+                .show();
+
+    }
